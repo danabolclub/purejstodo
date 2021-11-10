b6cd35080053ada2555ec32764d339f212a20193
danabolclub <danabolclub@yandex.ru>
2021-11-10 15:40:58 +0300

    'attempt'

--- JS/todo.js
+++ JS/todo.js
@@ -1,13 +1,56 @@
 const field = document.querySelector('.field');
+var arr = [];
+var lastnum = 0;
 
-function createTask(value) {
-	const task = document.createElement("div");
-	task.textContent = value;
-	return task;
+function createTask() {
+	lastnum++;
+	arr.push({
+		'number': lastnum,
+		'text': field.value,
+		'checked': false
+	});
+	field.value = '';
+	refreshList();
 }
 
-function addTask() {
-	const newTask = createTask(field.value);
-	document.body.appendChild(newTask);
+function deleteTask() {
+	var tasklist = document.querySelectorAll('div input');
+	for(var i = 0; i < tasklist.length; i++) {
+		if (tasklist[i].type == 'checkbox') {
+			for (let j = 0; j < arr.length; j++) {
+				if (arr[j].number == tasklist[i].id) {
+					arr[j].checked = tasklist[i].checked;
+				}
+			}
+    	}
+  	}
+	refreshList();
 }
 
+function logArray() {
+	for (let i = 0; i < arr.length; i++) {
+		console.log(arr[i]);
+	}
+}
+
+
+function refreshList() {
+	document.body.children[document.body.children.length - 2].remove();
+	const list = document.createElement('div');
+	list.className = 'list';
+	//task.class = '123123123';
+	document.body.children[document.body.children.length - 2].after(list);
+	for (let i = 0; i < arr.length; i++) {
+		if (arr[i].checked == false) {
+			checkbox = document.createElement('input');
+			checkbox.type = 'checkbox';
+			checkbox.id = arr[i].number;
+			label = document.createElement('label');
+			label.textContent = arr[i].text;
+			br = document.createElement('br');
+			document.body.children[document.body.children.length - 2].append(checkbox, label, br);
+		}
+	}
+
+
+}
